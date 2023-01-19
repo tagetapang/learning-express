7e2babec4f8758812fd5948ee30d14f71c8393e0
tage tapang <tagetage70@gmail.com>
2023-01-19 14:17:47 +0530

    did till 9:12

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
 const express = require("express")
 const app = express()
 const port = 5000;
+app.set('view engine', 'ejs')
 app.get('/',(req,res)=>{
     console.log("this is the get part");
-    res.send("this is the response")
+    res.render("index")
+    // res.send("this is the response")
 })
-app.listen(port)
+app.get('/download',(req,res)=>{
+    res.download('server.js')
+})
+app.listen(port,() => {
+    console.log(`app started in port http://localhost:${port}`)
+})
