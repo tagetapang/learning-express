57c4cf35b8de264f8fed59a9d330dc659eb26257
tage tapang <tagetage70@gmail.com>
2023-01-20 21:59:44 +0530

    Update middleware.js

--- middleware.js
+++ middleware.js
@@ -3,5 +3,6 @@ const express = require("express");
 const middleware = (req,res,next)=>{
     console.log(req.originalUrl);
     next()
+    console.log("wow this happend after the NEXT program")
 }
 module.exports = middleware
